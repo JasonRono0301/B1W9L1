17db923d23cd0cc488f87ad7e90fa1d16cc69092
Jason R <99057964@mydavinci.nl>
2020-10-26 12:23:10 +0100

    Bezig met opdracht 2.

--- opdracht 2/assets/opdracht2.js
+++ opdracht 2/assets/opdracht2.js
@@ -1,2 +1,9 @@
 //Blok 1 Week 9 Lab 1.
-//Opdracht 2 
+//Opdracht 2 26-10-2020.
+
+function addition(number1,number2){
+    return number1 + number2;
+}
+
+var uitkomst1 = addition(10, 22);
+document.write(uitkomst1);
