b3e48a12ac03ae7cd40878a2be12633a724cb6e7
Jason R <99057964@mydavinci.nl>
2020-10-26 11:48:40 +0100

    Opdracht 1 gemaakt en getest en voltooid.

---
+++ opdracht 1/assets/opdracht1.js
@@ -0,0 +1,12 @@
+//Jason Ronoastro.
+//Da Vinci College - Softwaredeveloper.
+//26-10-2020/Blok 1 Week 9 Lab 1.
+
+function HelloWorld(qty){
+    for(i=0; i<qty; i++){
+        document.write("Hello World" + "<br/>");
+    }
+}
+
+HelloWorld(3);
+
