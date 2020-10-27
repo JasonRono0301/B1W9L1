5772ae21a6427ce1fc48445047d3f6bc54736107
Jason R <99057964@mydavinci.nl>
2020-10-27 11:26:33 +0100

    Bezig met opdracht 2 heb al de eerste 2 sommen af.

--- opdracht 2/assets/opdracht2.js
+++ opdracht 2/assets/opdracht2.js
@@ -1,9 +1,26 @@
 //Blok 1 Week 9 Lab 1.
 //Opdracht 2 26-10-2020.
 
+var uitkomst;//Een globale variabele zodat ik deze in elke functie kan hergebruiken.
+
 function addition(number1,number2){
+    uitkomst = number1 + number2;
+    document.write(number1 + ' + ' + number2 + ' = ' + uitkomst + "<br>");
+    console.log(uitkomst);//Log voor de uitkomst.
     return number1 + number2;
 }
 
-var uitkomst1 = addition(10, 22);
-document.write(uitkomst1);
+addition(10,12);//Wat de waarden zijn in de parameters.
+
+function subtraction(number1, number2){
+    uitkomst = number1 - number2;
+    document.write(number1 + ' - ' + number2 + ' = ' + uitkomst + "<br>");
+    console.log(uitkomst);
+    return number1 - number2;
+}
+
+subtraction(58,34);
+
+function multiplication(number1, number2){
+    
+}
