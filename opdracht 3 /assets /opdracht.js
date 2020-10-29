2bf0aa561d00ef314e0d61ff5213d808144f5a7d
Jason R <99057964@mydavinci.nl>
2020-10-29 14:25:23 +0100

    Nog bezig met opdracht 3.

--- opdracht 3 /assets /opdracht.js
+++ opdracht 3 /assets /opdracht.js
@@ -1,6 +1,15 @@
 //Opdracht 3.
 //27-10-2020.
 
-function namen() {
+
+function namen(){
+    while(namen =! test){
+      var test =  prompt('TEST');
+        if (test = 'stop'){
+                    document.write('test');
+                }     
+    }
     
 }
+
+namen();
